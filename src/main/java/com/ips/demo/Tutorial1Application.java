e6edde787c8403d0166594946fcf5a111803f729
training178 <trainings178@gmail.com>
2020-03-18 19:12:51 -0500

    t

---
+++ src/main/java/com/ips/demo/Tutorial1Application.java
@@ -0,0 +1,13 @@
+package com.ips.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ips/demo/Tutorial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ips.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
